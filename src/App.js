ee43e3012c4fba367ec50c801a6b39fb0aefe4fd
Alex Keeley <keeley.lp@gmail.com>
2021-06-19 16:08:30 +0100

    Refactored app component and now displaying a background cover of one of the images within my assets, no repeat and covers the entire page span

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}));
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+    backgroundImage: `url(${
+      process.env.PUBLIC_URL + "/assets/guitarWall.jpg"
+    })`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+}));
 export default function App() {
   const classes = useStyles();
-  return (
-    <div>
-      <h1>Material UI Landing page</h1>
-    </div>
-  );
+  return <div className={classes.root}></div>;
 }
