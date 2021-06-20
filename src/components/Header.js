33dc918bd3e4f54e3b24cc39328a3983efdc27ad
Alex Keeley <keeley.lp@gmail.com>
2021-06-20 20:47:22 +0100

    Modified header component to include a toolbar and using flexbox to display the icon and title separately within a makestyles function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,39 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, IconButton } from "@material-ui/core";
+import { AppBar, IconButton, Toolbar } from "@material-ui/core";
 import SortIcon from "@material-ui/icons/Sort";
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  AppBar: {
+    background: "none",
+    fontFamily: "Newsreader",
+  },
+  icon: {
+    color: "#FFEED6",
+  },
+  header: {
+    color: "#FFEED6",
+    fontSize: "1.5em",
+    flexGrow: "1",
+  },
+  wrapper: {
+    width: "90%",
+    margin: "auto",
+  },
 }));
 
 export default function Header() {
   const classes = useStyles();
   return (
     <div>
-      <AppBar>Guitars</AppBar>
-      <IconButton>
-        <SortIcon></SortIcon>
-      </IconButton>
+      <AppBar className={classes.AppBar} elevation={0}>
+        <Toolbar className={classes.wrapper}>
+          <h2 className={classes.header}>Guitars</h2>
+          <IconButton>
+            <SortIcon className={classes.icon}></SortIcon>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
