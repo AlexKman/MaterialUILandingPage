3b87494f42dfab21153bbc81c1c8d982179ca2f4
Alex Keeley <keeley.lp@gmail.com>
2021-06-26 18:10:22 +0100

    Now displaying visited Card based on visitedPlaces array of data which provided the title, image url and description

---
+++ src/VisitedPlaces.js
@@ -0,0 +1,25 @@
+const VisitedPlaces = [
+  {
+    title: "Island 1",
+    description: "The favorite place I have visited, I cannot wait to go again",
+    imageUrl: process.env.PUBLIC_URL + "assets/visited/Island1.jpeg",
+  },
+  {
+    title: "Island 2",
+    description:
+      "Expensive, but definitely worth it, weather was amazing all week",
+    imageUrl: process.env.PUBLIC_URL + "assets/visited/Image2.jpeg",
+  },
+  {
+    title: "Island 3",
+    description: "Was so hot when I was there, perhaps too hot!",
+    imageUrl: process.env.PUBLIC_URL + "assets/visited/Island3.jpeg",
+  },
+  {
+    title: "Island 4",
+    description: "I almost drowned, would not recommend",
+    imageUrl: process.env.PUBLIC_URL + "assets/visited/Island4.jpeg",
+  },
+];
+
+export default VisitedPlaces;
--- src/components/Visited.js
+++ src/components/Visited.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import VisitedCard from "./Card";
+import VisitedPlaces from "../VisitedPlaces";
 
 const useStyles = makeStyles((theme) => ({
   visited: { height: "100vh" },
@@ -10,7 +11,7 @@ export default function () {
   const classes = useStyles();
   return (
     <div className={classes.visited}>
-      <VisitedCard></VisitedCard>
+      <VisitedCard placesVisited={VisitedPlaces[0]}></VisitedCard>
     </div>
   );
 }
